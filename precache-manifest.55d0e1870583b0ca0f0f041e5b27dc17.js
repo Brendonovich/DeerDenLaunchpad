efeb1ce407a4608dfab19e32bd34963a4ca10b88
Brendan Allan <brendonovich@outlook.com>
2020-06-17 16:03:04 +0800

    Updates

---
+++ precache-manifest.55d0e1870583b0ca0f0f041e5b27dc17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6fa3080827ea6fd82ff8df7db2a266",
+    "url": "/DeerDenLaunchpad/index.html"
+  },
+  {
+    "revision": "12bdc1a567ed89080b67",
+    "url": "/DeerDenLaunchpad/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d403ef43ae05e2dc702a",
+    "url": "/DeerDenLaunchpad/static/css/main.2287e7a1.chunk.css"
+  },
+  {
+    "revision": "12bdc1a567ed89080b67",
+    "url": "/DeerDenLaunchpad/static/js/2.bf446af3.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/DeerDenLaunchpad/static/js/2.bf446af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d403ef43ae05e2dc702a",
+    "url": "/DeerDenLaunchpad/static/js/main.a93d7062.chunk.js"
+  },
+  {
+    "revision": "eb7e324b686f9389a374",
+    "url": "/DeerDenLaunchpad/static/js/runtime-main.65d21477.js"
+  }
+]);
